8ab2da60a60a3cdaf4e8bbca96413f73c65241df
KKH <gyooheon@naver.com>
2024-05-24 09:59:46 +0900

    ✨ Feat : 메타데이터 컴포넌트화 및 링크 이미지 적용
    링크는 현재페이지 이미지는 유뷰트 썸네일이 들어가게 적용

--- src/pages/travel-information/[id].tsx
+++ src/pages/travel-information/[id].tsx
@@ -1,19 +1,8 @@
 import TravelInformtaionContent from '@/src/components/TravelInformationContent';
-import Head from 'next/head';
 
 const TravelInformation = () => {
   return (
     <>
-      <Head>
-        <title>uTrip</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-        <meta name="description" content={'상세페이지'} />
-        <meta property="og:image" content="https://i.ytimg.com/vi/or2TgTRjPq8/maxresdefault.jpg" />
-        <meta property="og:image:alt" content="alt" />
-        <meta property="og:url" content="www.naver.com" />
-        <meta property="og:title" content={'utrip: 상세페이지'} />
-        <meta property="og:type" content="website" />
-      </Head>
       <TravelInformtaionContent />
     </>
   );
