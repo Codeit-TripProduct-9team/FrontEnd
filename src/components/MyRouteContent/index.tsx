d4bfb8bd73228fbb5bf5ee1175f28c1b222cd547
정기환 <joelkh784@gmail.com>
2024-05-24 10:52:44 +0900

    ✨ Feat: add place button UI

--- src/components/MyRouteContent/index.tsx
+++ src/components/MyRouteContent/index.tsx
@@ -1,13 +1,18 @@
 import SearchBar from '../common/searchBar';
 import KakaoMap from './KakaoMap';
 import PlaceList from './PlaceList';
+import { PlusIcon } from '@heroicons/react/24/outline';
 
 const MyRouteContent = () => {
   return (
     <main className="flex gap-30 bg-gray-200 rounded-8">
-      <div className="bg-gray-50 p-20">
+      <div className="bg-gray-50 p-20 flex flex-col gap-10">
         <KakaoMap />
         <PlaceList />
+        <button className="w-full bg-blue-500 text-white rounded-8 p-15 flex justify-center items-center">
+          <PlusIcon className="w-20" />
+          일정 추가하기
+        </button>
       </div>
       <div>
         <SearchBar />
