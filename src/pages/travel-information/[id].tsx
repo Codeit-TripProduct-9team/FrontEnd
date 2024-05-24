babde5897ed6f843eab343bd9fb912f1c61d4a6b
KKH <gyooheon@naver.com>
2024-05-24 09:38:42 +0900

    ✨ Feat: opengraphy 메타 태그 추가
    공유를 위한 메타 데이터 추가

--- src/pages/travel-information/[id].tsx
+++ src/pages/travel-information/[id].tsx
@@ -1,7 +1,22 @@
 import TravelInformtaionContent from '@/src/components/TravelInformationContent';
+import Head from 'next/head';
 
 const TravelInformation = () => {
-  return <TravelInformtaionContent />;
+  return (
+    <>
+      <Head>
+        <title>uTrip</title>
+        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        <meta name="description" content={'상세페이지'} />
+        <meta property="og:image" content="https://i.ytimg.com/vi/or2TgTRjPq8/maxresdefault.jpg" />
+        <meta property="og:image:alt" content="alt" />
+        <meta property="og:url" content={window.location.href} />
+        <meta property="og:title" content={'utrip: 상세페이지'} />
+        <meta property="og:type" content="website" />
+      </Head>
+      <TravelInformtaionContent />
+    </>
+  );
 };
 
 export default TravelInformation;
