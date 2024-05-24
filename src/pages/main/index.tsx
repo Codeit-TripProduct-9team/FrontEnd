7b6fb8838fd2085a91c805b55fc6cc7ea2fe4d51
parkwoohyeok <dngur3111@naver.com>
2024-05-24 17:03:32 +0900

    :sparkles: Feat: 검색 인풋 ui 구현

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,24 +1,24 @@
 import CarouselSection from '@/src/components/mainContent/CarouselSection';
 // import ListCard from '@/src/components/mainContent/ListCard';
-// import ListSearchInput from '@/src/components/mainContent/ListSearchInput';
+import ListSearchInput from '@/src/components/mainContent/ListSearchInput';
 import { mock } from '@/src/components/mainContent/mock';
 
 const main = () => {
   return (
-    <div className="flex flex-col  p-100">
+    <div className="flex flex-col items-center p-100">
       <header>
         <CarouselSection data={mock.data} />
       </header>
-      {/* <main>
+      <main>
         <article>
           <ListSearchInput />
-          <div>
-            <h1>검색유도문구</h1>
-            <p>description</p>
-            <ListCard/>
+          <div className="mt-20">
+            <h1 className="text-center text-20 font-bold">검색유도문구</h1>
+            <p className="text-center text-15">description</p>
           </div>
         </article>
-      </main> */}
+        {/* <ListCard/> */}
+      </main>
     </div>
   );
 };
