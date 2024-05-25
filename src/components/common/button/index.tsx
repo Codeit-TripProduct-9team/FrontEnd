5adf691c5e284d573fd9f703ed4ba73075c6e32d
정기환 <joelkh784@gmail.com>
2024-05-25 14:05:15 +0900

    🐛 Fix: button component style override error

--- src/components/common/button/index.tsx
+++ src/components/common/button/index.tsx
@@ -3,11 +3,9 @@ import { twMerge } from 'tailwind-merge';
 
 type CustomStyle = {
   children: ReactNode;
-  bgColor?: string;
-  textColor?: string;
   disabled?: boolean;
-  rounded?: number | string;
   onClick?: () => void;
+  textColor?: string;
   className?: string;
 };
 
@@ -15,27 +13,17 @@ type CustomButtonProps = CustomStyle & ButtonHTMLAttributes<HTMLButtonElement>;
 
 const Button: React.FC<CustomButtonProps> = ({
   children,
-  bgColor,
-  textColor,
-  rounded,
   disabled,
   onClick,
-  className,
+  textColor = '',
+  className = '',
   ...props
 }) => {
-  //twMerge에서 dynamic classNames을 지원 안하기 때문에 이런식으로 작성했음
-  const defaultStyle = 'w-408 h-54 text-18 flex justify-center items-center ';
-  const buttonClass = twMerge(`${defaultStyle} ${className}`);
-  const bgColorClass = bgColor ? `bg-${bgColor}` : 'bg-blue';
-  const textColorClass = textColor ? `text-${textColor}` : 'text-white';
-  const roundedClass = rounded ? `rounded-${rounded}` : 'rounded-s';
+  const buttonStyle = twMerge('w-408 h-54 text-18 flex justify-center items-center bg-blue rounded-10', className);
+  const textColorStyle = textColor ? `text-${textColor}` : 'text-white';
+
   return (
-    <button
-      className={`${bgColorClass} ${textColorClass} ${roundedClass} ${buttonClass}`}
-      disabled={disabled}
-      onClick={onClick}
-      {...props}
-    >
+    <button className={`${buttonStyle} ${textColorStyle}`} disabled={disabled} onClick={onClick} {...props}>
       {children}
     </button>
   );
