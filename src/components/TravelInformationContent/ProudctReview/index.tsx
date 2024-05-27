68097e8cc6abec02075663b26dee129a61c14e63
KKH <gyooheon@naver.com>
2024-05-27 13:54:26 +0900

    ✨ Feat: 별점 기준으로 하는 정렬 기능 추가
    별점 기준으로 리뷰 정렬해주는 기능 추가

--- src/components/TravelInformationContent/ProudctReview/index.tsx
+++ src/components/TravelInformationContent/ProudctReview/index.tsx
@@ -1,25 +1,30 @@
 import { useState } from 'react';
 
 import ReviewList from './ReviewList';
-import SortToolbar from './SortToolbar/indes';
+import SortToolbar from './SortToolbar';
 import { reviewData } from './mock';
 
 const ProductReview = () => {
   const [sortedReview, setSortedReview] = useState(reviewData);
 
-  const sortByNewest = () => {
+  const handleSortByNewest = () => {
     const sorted = [...sortedReview].sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
     setSortedReview(sorted);
   };
 
-  const sortByLikes = () => {
+  const handleSortByLikes = () => {
     const sorted = [...sortedReview].sort((a, b) => b.likes - a.likes);
+    setSortedReview(sorted);
+  };
+
+  const handleSrotByScore = () => {
+    const sorted = [...sortedReview].sort((a, b) => b.score - a.score);
     setSortedReview(sorted);
   };
 
   return (
     <div className="flex flex-col gap-40">
-      <SortToolbar sortByNewest={sortByNewest} sortByLikes={sortByLikes} />
+      <SortToolbar sortByNewest={handleSortByNewest} sortByLikes={handleSortByLikes} sortByScore={handleSrotByScore} />
       <ReviewList sortedReview={sortedReview} />
     </div>
   );
