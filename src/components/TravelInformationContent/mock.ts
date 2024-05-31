e1a6ba2d4e1f0ad0cbb0c3f20a49649b991b228a
KKH <gyooheon@naver.com>
2024-05-31 14:15:24 +0900

    ✨ Feat: 공유 기능 추가
    공유 기능 추가

--- src/components/TravelInformationContent/mock.ts
+++ src/components/TravelInformationContent/mock.ts
@@ -7,6 +7,5 @@ export const youtubeMockData = {
   description:
     '백종원이 예산시장에 떴다~! [님아 그시장을 가오 158화]에서 소개된 충남 예산시장에 서 먹부림도 부리고~ 주변에서 들를만한 코스를 추가해서 나만의 여행 코스를 만들어보세요!',
   youtuber: '백종원 PAIK JONG WON',
-  updatedAt: '2024. 4. 18.',
   tag: ['🛤️충남', '🧺시장', '먹방🌭'],
 };
--- src/utils/convertDate.ts
+++ src/utils/convertDate.ts
@@ -1,7 +1,7 @@
 const convertDate = (dateString: string) => {
   const date = new Date(dateString);
   const year = date.getFullYear();
-  const month = date.getMonth() + 1; // getMonth() returns month from 0-11
+  const month = date.getMonth() + 1;
   const day = date.getDate();
 
   return `${year}. ${month}. ${day}.`;
