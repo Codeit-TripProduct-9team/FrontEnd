a07f1231037e805dfb7d8ef6b0437589ad8ce5b9
KKH <gyooheon@naver.com>
2024-06-01 10:26:47 +0900

    🩹 Fix : 위치를 지도에서 검색했을 때 시작위치 마커와 경로가 클릭 후 표시되게 수정
    지도에 위치 표시가 원하는 시점에 되게 수정

--- src/components/TravelInformationContent/ProductDescription/CustomLocation/index.tsx
+++ src/components/TravelInformationContent/ProductDescription/CustomLocation/index.tsx
@@ -1,11 +1,12 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useState } from 'react';
+
+import LocationInput from './LocationInput';
 
 import convertTime from '@/src/utils/convertTime';
 import getDirectionRequest from '@/src/utils/getDirectionRequest';
 import { KAKAO_ROAD_BASED_URL } from '@/src/constants/url';
 import extractPath from '@/src/utils/extractPath';
 import instance from '@/src/api/axios';
-import LocationInput from './LocationInput';
 
 interface ElaspedTimeProps {
   destinationName: string;
@@ -24,32 +25,27 @@ const CustomLocation = ({
   const [duration, setDuration] = useState(0);
   const [showMessage, setShowMessage] = useState(false);
   const [invalidKeyword, setInvalidKeyword] = useState(0);
-  const [customStartingPoint, setCustomStartingPoint] = useState('');
-  const [coordinate, setCoordinate] = useState({ lng: '', lat: '' });
-  const [customRoute, setCustomRoute] = useState<{ lat: number; lng: number }[]>([]);
+  const [customLocation, setCustomLocation] = useState('');
 
-  const getCoordinate = useCallback(
-    async (address: string) => {
-      const encodedAddress = encodeURIComponent(address);
-      try {
-        const response = await instance.get(`${KAKAO_ROAD_BASED_URL}${encodedAddress}`, {
-          headers: {
-            Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
-          },
-        });
-        const result = await response.data;
-        const customLocation = result.documents[0];
-        const coordinate = { lat: customLocation.y, lng: customLocation.x };
-        setCoordinate(coordinate);
-        setCustomStartPoint(coordinate);
-      } catch (error) {
-        console.error('Error:', error);
-      }
-    },
-    [setCustomStartPoint],
-  );
+  const getCoordinate = async (address: string) => {
+    const encodedAddress = encodeURIComponent(address);
+    try {
+      const response = await instance.get(`${KAKAO_ROAD_BASED_URL}${encodedAddress}`, {
+        headers: {
+          Authorization: `KakaoAK ${process.env.NEXT_PUBLIC_KAKAO_REST_API_KEY}`,
+        },
+      });
+      const result = await response.data;
+      const customLocation = result.documents[0];
+      const coordinate = { lat: parseFloat(customLocation.y), lng: parseFloat(customLocation.x) };
+      return coordinate;
+    } catch (error) {
+      console.error('Error:', error);
+      return null;
+    }
+  };
 
-  const getDirection = useCallback(async () => {
+  const getDirection = async (coordinate: { lat: number; lng: number }) => {
     const { requestUrl, headers } = getDirectionRequest(coordinate, destinationPosition);
     try {
       const response = await instance.get(requestUrl, {
@@ -60,60 +56,52 @@ const CustomLocation = ({
       const elapsedTime = result.routes[0].summary.duration;
 
       const path = extractPath(result);
-      setCustomRoute(path);
       setDuration(elapsedTime);
       setInvalidKeyword(result.totalCount);
+      return path;
     } catch (error) {
       console.error('Error:', error);
+      return [];
     }
-  }, [coordinate, destinationPosition]);
-
-  useEffect(() => {
-    const hasLocation = customStartingPoint.trim() !== '';
-    if (hasLocation) {
-      getCoordinate(customStartingPoint);
-    }
-  }, [customStartingPoint, getCoordinate]);
-
-  useEffect(() => {
-    const hasCoordinate = coordinate.lng !== '' && coordinate.lat !== '';
-    if (hasCoordinate) {
-      getDirection();
-    }
-  }, [coordinate, getDirection]);
+  };
 
   const handleChangeStartingPoint = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const customLocation = event.target.value;
+    const location = event.target.value;
     if (showMessage) {
       setShowMessage(false);
     }
-    setCustomStartingPoint(customLocation);
+    setCustomLocation(location);
   };
 
-  const handleStartingPoint = () => {
-    const hasLocation = customStartingPoint.trim() !== '';
+  const handleStartingPoint = async () => {
+    const hasLocation = customLocation.trim() !== '';
     if (hasLocation) {
-      setShowMessage(true);
-      setPolylinePath(customRoute);
+      const coordinate = await getCoordinate(customLocation);
+      if (coordinate) {
+        const path = await getDirection(coordinate);
+        setShowMessage(true);
+        setPolylinePath(path);
+        setCustomStartPoint(coordinate);
+      }
+      if (!coordinate) {
+        setShowMessage(true);
+        setInvalidKeyword(0);
+      }
     }
   };
 
   const elapsedTime = convertTime(duration);
 
   return (
-    <div className="absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col gap-4  w-582 p-10 z-10 text-center rounded-s">
-      <LocationInput
-        location={customStartingPoint}
-        onChange={handleChangeStartingPoint}
-        onClick={handleStartingPoint}
-      />
+    <div className="absolute top-20 left-1/2 transform -translate-x-1/2 flex flex-col gap-4 w-582 p-10 z-10 text-center rounded-s">
+      <LocationInput location={customLocation} onChange={handleChangeStartingPoint} onClick={handleStartingPoint} />
       {showMessage && (
-        <div className="p-10  rounded-s bg-white">
+        <div className="p-10 rounded-s bg-white">
           {invalidKeyword === 0 ? (
             <p>잘못된 주소이거나 거리가 너무 가깝습니다</p>
           ) : (
             <p>
-              {customStartingPoint}에서 {destinationName}까지 {`${elapsedTime.hours} 시간 ${elapsedTime.minutes} 분`}
+              {customLocation}에서 {destinationName}까지 {`${elapsedTime.hours} 시간 ${elapsedTime.minutes} 분`}
               걸려요💨
             </p>
           )}
