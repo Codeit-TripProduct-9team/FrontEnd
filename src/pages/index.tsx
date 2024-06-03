060c4e8b098e60b9e5dcb5266d2cf91658ee3e4c
parkwoohyeok <dngur3111@naver.com>
2024-06-04 01:33:03 +0900

    배포에러 삭제

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,8 @@
 import Layout from '../components/common/layout';
 import CarouselSection from '@/src/components/mainContent/CarouselSection';
 import ListSearchSection from '@/src/components/mainContent/ListSearchSection';
-import { useEffect, useState } from 'react';
 
 const main = () => {
-  const [mounted, setMounted] = useState<boolean>(false);
-
-  useEffect(() => {
-    setMounted(true);
-  }, []);
-
-  if (!mounted) return;
   return (
     <Layout>
       <div className="flex flex-col items-center py-50 ">
