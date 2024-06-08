ad4ed8cb28ac60b14ac5fa665e6d611c0c15def9
KKH <gyooheon@naver.com>
2024-06-08 13:11:38 +0900

    ✏️ Fix: 페이지 탭 전환하면 새로고침 되는 문제 해결
    탭을 전환하면 페이지가 초기화 되는 문제 해결

--- src/hooks/useSelectContent.ts
+++ src/hooks/useSelectContent.ts
@@ -1,20 +1,10 @@
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 const useSelectContent = (initialContent: string) => {
   const [content, setContent] = useState(initialContent);
-  const route = useRouter();
-
-  useEffect(() => {
-    const { content: queryContent } = route.query;
-    if (typeof queryContent === 'string') {
-      setContent(queryContent);
-    }
-  }, [route.query]);
 
   const handleChangeContent = (newContent: string) => {
     setContent(newContent);
-    route.push({ pathname: route.pathname, query: { ...route.query, content: newContent } });
   };
 
   return { content, handleChangeContent };
