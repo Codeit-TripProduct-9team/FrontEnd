df0d7e70414b6bf72dd52856453e5b156623dd9a
KKH <gyooheon@naver.com>
2024-06-11 19:33:36 +0900

    ✨ Feat:상세페이지 검색  스켈레톤 추가
    상세 페이지 검색 영역 스켈레톤 ui 추가

--- src/components/TravelInformationContent/SearchBar/SearchContent/indext.tsx
+++ src/components/TravelInformationContent/SearchBar/SearchContent/indext.tsx
@@ -4,6 +4,7 @@ import SearchInformation from './SearchInformation';
 
 import { videoListProps } from '@/src/lib/types';
 import getYoutubeData from '@/src/api/getYoutubeData';
+import InformationSearchSkeleton from '@/src/components/common/skeleton/InformationSearchSkeleton';
 
 interface SearchContentProps {
   searchResult: videoListProps[];
@@ -19,7 +20,7 @@ const SearchContent = ({ searchResult, onClick }: SearchContentProps) => {
       for (const { id, url } of searchResult) {
         const videoId = url.split('v=')[1];
         const data = await getYoutubeData(videoId);
-        const thumbnail = data?.items[0]?.snippet?.thumbnails?.default?.url;
+        const thumbnail = data?.items[0]?.snippet?.thumbnails?.high?.url;
         if (thumbnail) {
           thumbnailsData[id] = thumbnail;
         }
@@ -31,19 +32,27 @@ const SearchContent = ({ searchResult, onClick }: SearchContentProps) => {
   }, [searchResult]);
 
   return (
-    <ul className="absolute flex flex-col left-120 top-65 py-20 w-625 h-290 bg-gray-20 border-2 border-gray-50 overflow-y-scroll">
+    <ul className="absolute flex flex-col left-120 top-65 pt-20  w-625 h-280 bg-gray-20 border-2 border-gray-50 overflow-y-scroll">
       {searchResult.map(({ id, title, tag }) => (
         <li
           key={id}
           className="flex w-full gap-24 px-24 py-12 text-18 text-gray-50 cursor-pointer hover:bg-gray-30"
           onClick={() => onClick(id)}
         >
-          <div className="flex items-center h-87 w-87 overflow-hidden rounded-10">
-            {thumbnails[id] && (
-              <Image className="object-cover h-87" src={thumbnails[id]} alt="img" width={87} height={87} />
-            )}
-          </div>
-          <SearchInformation title={title} tag={tag} />
+          {thumbnails[id] ? (
+            <>
+              <Image
+                className="object-cover h-87 rounded-m overflow-hidden"
+                src={thumbnails[id]}
+                alt="img"
+                width={87}
+                height={87}
+              />
+              <SearchInformation title={title} tag={tag} />
+            </>
+          ) : (
+            <InformationSearchSkeleton />
+          )}
         </li>
       ))}
     </ul>
