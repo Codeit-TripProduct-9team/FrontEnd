e763936c4c927bfe571c610b8a6f1a2997d33f06
KKH <gyooheon@naver.com>
2024-06-14 02:18:57 +0900

    💄 Style : 화면 크기 줄어들면 이미지 꺠지는 부분 수정
    화면 크기 작아지면 이미지 꺠지는 부분 최소 사이즈 지정하여 수정

--- src/components/TravelInformationContent/ProductInformation/VideoInformation/index.tsx
+++ src/components/TravelInformationContent/ProductInformation/VideoInformation/index.tsx
@@ -12,7 +12,7 @@ const YoutubeData = ({ youtubeData, youtubeId }: YoutubeDataProps) => {
   const { viewCount, updatedAt, thumbnail } = useYouTubeData(youtubeId);
 
   return (
-    <div className="flex flex-col w-full h-378 gap-24">
+    <div className="flex flex-col min-w-800 w-full h-378 gap-24">
       <h1 className="text-28 font-bold">{youtubeData?.title}</h1>
       <div className="flex flex-col gap-12 text-gray-70">
         <p className="text-20 text-gray-50">{youtubeData?.content}</p>
