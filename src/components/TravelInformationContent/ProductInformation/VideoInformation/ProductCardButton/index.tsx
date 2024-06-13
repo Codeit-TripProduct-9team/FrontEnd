9b788cf8d182093323c4e8aa223d82173ed44cf2
KKH <gyooheon@naver.com>
2024-06-14 01:38:40 +0900

    ✨ Feat: 마이플레이스 등록 여부에 따른 버튼 메세지 색상 변경
    마이플레이스 등록 여부에 따른 ui변경

--- src/components/TravelInformationContent/ProductInformation/VideoInformation/ProductCardButton/index.tsx
+++ src/components/TravelInformationContent/ProductInformation/VideoInformation/ProductCardButton/index.tsx
@@ -13,6 +13,8 @@ import { currentPageUrl, shareFacebook, shareKakao, shareTwitter } from '@/src/u
 import instance from '@/src/api/axios';
 import { useRouter } from 'next/router';
 import { getCookie } from '@/src/utils/cookie';
+import toast from 'react-hot-toast';
+import { useState } from 'react';
 
 interface ProductButtonProps {
   title: string | undefined;
@@ -21,9 +23,12 @@ interface ProductButtonProps {
 }
 
 const ProductCardButton = ({ title, description, thumbnail }: ProductButtonProps) => {
-  const hasToken = getCookie('accessToken');
+  const [isLike, setIsLike] = useState(false);
+
   const route = useRouter();
   const videoId = route.query.id as string;
+
+  const hasToken = getCookie('accessToken');
 
   const sharedOverlay = useOverlay();
   const sharedOnModal = () => {
@@ -46,21 +51,43 @@ const ProductCardButton = ({ title, description, thumbnail }: ProductButtonProps
     };
     try {
       const response = await instance.post(`/video/${videoId}/likes`, body, { headers });
-      console.log(response);
+      if (response.status === 200) {
+        toast.success('나의 코스에 등록되었습니다!');
+        setIsLike(true);
+      }
     } catch (error) {
       console.error(error);
+    }
+  };
+
+  const handleRouteCustomCourse = async () => {
+    if (!isLike) {
+      await handleRegistMyPlace();
+      setTimeout(() => {
+        route.push('/my-page');
+      }, 500);
+    }
+
+    if (isLike) {
+      setTimeout(() => {
+        route.push('/my-page');
+      }, 500);
     }
   };
 
   return (
     <div className="flex gap-12">
       <Link href="/my-route">
-        <Button className="bg-blue w-134 h-39 text-18 font-bold" textColor={'white'} onClick={handleRegistMyPlace}>
+        <Button className="bg-blue w-134 h-39 text-18 font-bold" textColor={'white'} onClick={handleRouteCustomCourse}>
           지금 코스짜기
         </Button>
       </Link>
-      <Button className="bg-blue w-161 h-39 text-18 font-bold" textColor={'white'} onClick={handleRegistMyPlace}>
-        마이플레이스 등록
+      <Button
+        className={`${isLike ? 'bg-red' : 'bg-blue'} w-161 h-39 text-18 font-bold`}
+        textColor={'white'}
+        onClick={handleRegistMyPlace}
+      >
+        {isLike ? '마이플레이스 삭제' : '마이플레이스 등록'}
       </Button>
       <button className="flex items-center py-6 px-16 rounded-s bg-gray-10" onClick={sharedOnModal}>
         <Image src={shareIcon} alt="share" width={27} height={27} />
