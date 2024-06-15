afd91fb4a10f87d752f280a80a3a4d059d1bd605
KKH <gyooheon@naver.com>
2024-06-15 10:21:11 +0900

    🐛 Fix: 오토 포커스가 들어가있어서 화면 전환시 화면이 아래로 내려가는 문제 수정
    오토 포커스 삭제

--- src/components/TravelInformationContent/ProductDescription/CustomLocation/LocationInput/index.tsx
+++ src/components/TravelInformationContent/ProductDescription/CustomLocation/LocationInput/index.tsx
@@ -1,8 +1,6 @@
 import Image from 'next/image';
 
 import SearchIcon from '@/public/assets/icon/search.png';
-import useAutoFocus from '@/src/hooks/useAtuoFocus';
-import { useRef } from 'react';
 
 interface LocationInputProps {
   location: string;
@@ -11,14 +9,9 @@ interface LocationInputProps {
 }
 
 const LocationInput = ({ location, onChange, onClick }: LocationInputProps) => {
-  const focusRef = useRef<HTMLInputElement>(null);
-
-  useAutoFocus(focusRef);
-
   return (
     <div className="relative p-10 rounded-s bg-white z-10">
       <input
-        ref={focusRef}
         className="placeholder:text-gray-60 font-bold"
         placeholder="지금 계신 곳을 입력해 주세요!"
         value={location}
