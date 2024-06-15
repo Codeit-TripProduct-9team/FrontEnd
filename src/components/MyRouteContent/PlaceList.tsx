78c34360caa0b6be9548d9c1053bae2429487b92
정기환 <joelkh784@gmail.com>
2024-06-15 15:18:09 +0900

    ✨ Feat: add day UI and logic

--- src/components/MyRouteContent/PlaceList.tsx
+++ src/components/MyRouteContent/PlaceList.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import PlaceItem from './PlaceItem';
 import { Droppable } from '@hello-pangea/dnd';
 import { useCourseStore } from '@/src/utils/zustand/useCourseStore';
+import { openToast } from '@/src/utils/openToast';
 
 export type PlaceList = {
   day: number;
@@ -17,9 +18,23 @@ export type Place = {
 
 const PlaceList = () => {
   const courseData = useCourseStore((state) => state.data.course[0].plan);
+  const { addDay } = useCourseStore();
+  const courseId = 1;
+  const maxDay = 7;
+  const newDay = { day: courseData.length + 1, place: [] };
+  const handleAddDay = () => {
+    if (courseData.length >= maxDay) {
+      openToast.error('최대 7일까지만 추가할 수 있습니다.');
+    } else {
+      addDay(courseId, newDay);
+    }
+  };
 
   return (
     <div className="relative mb-20 overflow-y-auto h-460 scrollbar-hide">
+      <div className="flex justify-center items-center text-12 text-gray-50 rounded-s mt-10">
+        드래그앤 드랍으로 마음에 드는 여행지를 내 계획에 포함해보세요
+      </div>
       <div className="border-l-2 absolute top-10 -left-20 border-gray-60 border-dashed h-[92%]" />
       {courseData.map((data) => (
         <div key={data.day} className="my-20 relative">
@@ -37,9 +52,12 @@ const PlaceList = () => {
           </Droppable>
         </div>
       ))}
-      <div className="flex justify-center items-center relative text-12 text-gray-50 w-441 h-60 border-1 rounded-s border-dashed border-gray-40">
+      <div
+        className="flex justify-center items-center relative text-14 text-gray-50 w-441 h-60 border-1 rounded-s border-dashed border-gray-40 cursor-pointer"
+        onClick={handleAddDay}
+      >
         <div className="w-10 h-10 bg-gray-60 rounded-full absolute -left-25 top-20" />
-        드래그앤 드랍으로 마음에 드는 여행지를 내 계획에 포함해보세요
+        일차 추가하기 +
       </div>
     </div>
   );
