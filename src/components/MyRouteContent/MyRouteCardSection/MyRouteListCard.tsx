3d8fbf063359d35688b2dcf4fde2093b52f21a1b
정기환 <joelkh784@gmail.com>
2024-06-20 23:04:54 +0900

    ✨ Feat: flippable my place card

--- src/components/MyRouteContent/MyRouteCardSection/MyRouteListCard.tsx
+++ src/components/MyRouteContent/MyRouteCardSection/MyRouteListCard.tsx
@@ -1,19 +1,17 @@
-// import { MockDataItem } from '@/src/lib/types';
-import { MyPlaceCardData } from '@/src/lib/types';
+import { MyPlace } from '@/src/lib/types';
 import Image from 'next/image';
-import Link from 'next/link';
 import { Draggable } from '@hello-pangea/dnd';
 import { openToast } from '@/src/utils/openToast';
 import { TOAST_MESSAGE } from '@/src/constants/constants';
 import useYouTubeData from '@/src/hooks/useYouTubeData';
+import truncateText from '@/src/utils/truncateText';
 
 interface ListCardProps {
-  data: MyPlaceCardData;
+  data: MyPlace;
 }
 
 const MyRouteListCard = ({ data }: ListCardProps) => {
-  console.log('data', data);
-  const { content, description, img, name, id, posX, posY, tags, title, videoUrl } = data;
+  const { description, img, name, id, tags, title, videoUrl } = data;
   const videoId = videoUrl.split('v=')[1];
   const { thumbnail } = useYouTubeData(videoId);
 
@@ -28,43 +26,49 @@ const MyRouteListCard = ({ data }: ListCardProps) => {
           ref={provided.innerRef}
           {...provided.draggableProps}
           {...provided.dragHandleProps}
-          // className="flex flex-col overflow-hidden bg-white w-225 h-225 rounded-s shadow-main transition-transform duration-300 transform hover:scale-105 cursor-pointer"
-
-          className="flex flex-col  bg-white  rounded-s shadow-main transition-transform duration-300 transform hover:scale-105 cursor-pointer"
+          className="flex flex-col overflow-hidden bg-white w-225 h-225 rounded-s shadow-main cursor-pointer"
         >
-          <div className="relative border-1 w-225 h-120">
-            {thumbnail && <Image src={thumbnail} fill alt="썸네일" priority className="object-cover object-cetner" />}
-          </div>
-          <div className="flex flex-col justify-between p-10 h-110">
-            <div>
-              <h2 className="font-bold text-13 mb-5 overflow-ellipsis-2">{title}</h2>
-            </div>
-            <div className="flex justify-between">
-              <div className="flex gap-5">
-                {tags?.slice(0, 2).map((tags: string, index: number) => (
-                  <div className="flex rounded-s font-bold bg-gray-10 py-3 px-10 text-12" key={index}>
-                    {tags}
-                  </div>
-                ))}
-              </div>
-              <div
-                className="transition-transform duration-300 transform hover:scale-105 hover:bg-gray-30 rounded-s font-bold bg-gray-10 py-3 px-10 text-8"
-                onClick={handleDeleteMyPlace}
-              >
-                마이플레이스 삭제
-              </div>
-            </div>
-          </div>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front">
+                <div className="relative border-1 w-225 h-120">
+                  {thumbnail && (
+                    <Image src={thumbnail} fill alt="썸네일" priority className="object-cover object-cetner" />
+                  )}
+                </div>
 
-          <div className="relative border-1 w-225 h-120">
-            {img && <Image src={img} fill alt="장소" priority className="object-cover object-cetner" />}
-          </div>
-          <div className="flex flex-col justify-between p-10 h-110">
-            <div>
-              <h2 className="font-bold text-13 mb-5 overflow-ellipsis-2">{name}</h2>
-            </div>
-            <div className="flex justify-between">
-              <p className="text-12">{description}</p>
+                <div className="flex flex-col justify-between p-10 h-110">
+                  <div>
+                    <h2 className="font-bold text-13 mb-5 overflow-ellipsis-2">{title}</h2>
+                  </div>
+                  <div className="flex justify-between">
+                    <div className="flex gap-5">
+                      {tags?.slice(0, 2).map((tags: string, index: number) => (
+                        <div className="flex rounded-s font-bold bg-gray-10 py-3 px-10 text-12" key={index}>
+                          {tags}
+                        </div>
+                      ))}
+                    </div>
+                    <div
+                      className="transition-transform duration-300 transform hover:scale-105 hover:bg-gray-30 rounded-s font-bold bg-gray-10 py-3 px-10 text-8"
+                      onClick={handleDeleteMyPlace}
+                    >
+                      마이플레이스 삭제
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="flip-card-back">
+                <div className="relative border-1 w-225 h-120">
+                  {img && <Image src={img} fill alt="장소" priority className="object-cover object-cetner" />}
+                </div>
+                <div className="flex flex-col p-10 h-110">
+                  <div>
+                    <h2 className="font-bold text-13 mb-5 overflow-ellipsis-2">{name}</h2>
+                  </div>
+                  <p className="text-12">{truncateText(description, 70)}</p>
+                </div>
+              </div>
             </div>
           </div>
         </div>
