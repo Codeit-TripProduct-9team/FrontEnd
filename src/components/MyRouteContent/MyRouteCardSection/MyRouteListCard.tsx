9ba8c3d2c3f58135f02e11225be3be9dfc08a7ce
정기환 <joelkh784@gmail.com>
2024-06-22 13:32:04 +0900

    ♻️ Refactor: remove delete my place

--- src/components/MyRouteContent/MyRouteCardSection/MyRouteListCard.tsx
+++ src/components/MyRouteContent/MyRouteCardSection/MyRouteListCard.tsx
@@ -2,8 +2,6 @@ import { CardDataItem } from '@/src/lib/types';
 
 import Image from 'next/image';
 import { Draggable } from '@hello-pangea/dnd';
-import { openToast } from '@/src/utils/openToast';
-import { TOAST_MESSAGE } from '@/src/constants/constants';
 import useYouTubeData from '@/src/hooks/useYouTubeData';
 import truncateText from '@/src/utils/truncateText';
 
@@ -15,10 +13,6 @@ const MyRouteListCard = ({ data }: ListCardProps) => {
   const { description, img, name, id, tags, title, videoUrl } = data;
   const videoId = videoUrl.split('v=')[1];
   const { thumbnail } = useYouTubeData(videoId);
-
-  const handleDeleteMyPlace = () => {
-    openToast.success(TOAST_MESSAGE.DELETE);
-  };
 
   return (
     <Draggable draggableId={`${name}`} index={id}>
@@ -49,12 +43,6 @@ const MyRouteListCard = ({ data }: ListCardProps) => {
                           {tags}
                         </div>
                       ))}
-                    </div>
-                    <div
-                      className="transition-transform duration-300 transform hover:scale-105 hover:bg-gray-30 rounded-s font-bold bg-gray-10 py-3 px-10 text-8"
-                      onClick={handleDeleteMyPlace}
-                    >
-                      마이플레이스 삭제
                     </div>
                   </div>
                 </div>
