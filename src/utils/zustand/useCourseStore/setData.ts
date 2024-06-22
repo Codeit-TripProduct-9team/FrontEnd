7bf3f3dde98e8702abce393ede368d936ff3f5b7
정기환 <joelkh784@gmail.com>
2024-06-22 14:27:50 +0900

    ♻️ Refactor: separate reorder index logic

--- src/utils/zustand/useCourseStore/setData.ts
+++ src/utils/zustand/useCourseStore/setData.ts
@@ -1,6 +1,6 @@
 import { Course } from '@/src/lib/types';
-import { Plan } from '@/src/lib/types';
 import { CourseStore } from './useCourseStore';
+import reorderIndex from '../../reorderIndex';
 
 interface SetDataProps {
   (state: CourseStore, data: Course): CourseStore;
@@ -9,13 +9,7 @@ interface SetDataProps {
 const setData: SetDataProps = (state, data) => {
   const newPlan = [...data.plan];
 
-  // Reassign index values in ascending order
-  let globalIndex = 1;
-  for (let j = 0; j < newPlan.length; j++) {
-    for (let i = 0; i < newPlan[j].place.length; i++) {
-      newPlan[j].place[i].index = globalIndex++;
-    }
-  }
+  reorderIndex(newPlan);
   return { ...state, data: { ...state.data, plan: newPlan } };
 };
 
