04b7aec7f85aae33dde049d00e508756adace8a0
naeun14 <mimimi303@naver.com>
2024-06-23 21:17:03 +0900

    ✏️ Fix: img url 받아오기 수정

--- src/components/MyCourseContent/LoadMoreModal/PlaceOfData/index.tsx
+++ src/components/MyCourseContent/LoadMoreModal/PlaceOfData/index.tsx
@@ -1,19 +1,11 @@
 import { Place } from '@/src/lib/types';
 import ConditionalImage from '../../ConditionalImage';
+import getValidImageUrl from '@/src/utils/getValidImageUrl';
 
 type PlaceOfDataProps = {
   data: Place;
 };
 const PlaceOfData = ({ data }: PlaceOfDataProps) => {
-  const getValidImageUrl = (url: string) => {
-    try {
-      const parsedUrl = new URL(url);
-      return parsedUrl.href;
-    } catch (e) {
-      console.error('Invalid image URL', url);
-      return '';
-    }
-  };
   return (
     <div className="flex flex-row justify-between items-center mb-5">
       <p className="p-10 ">
