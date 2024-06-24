a8bf01b1cdeb4a56f0b644d8dfdbc779b7061943
정기환 <joelkh784@gmail.com>
2024-06-24 23:37:46 +0900

    💄 Style: 404 page styling

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -10,7 +10,7 @@ const NoPage = () => {
         <div className="mb-20">
           <Image src={loginLogo} width={200} height={200} alt="로그인로고" />
         </div>
-        <h1 className="text-60 text-center">존재하지 않는 페이지 입니다.</h1>
+        <h1 className="text-30 text-center font-bold text-gray-60">존재하지 않는 페이지 입니다.</h1>
         <div className="flex mt-30 bg-blue text-20 items-center justify-center gap-10 cursor-pointer text-white p-10 rounded-s ">
           <div onClick={() => router.push('/')}>홈으로 가기</div>
         </div>
